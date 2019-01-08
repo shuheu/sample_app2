825ec13e3557ab4a326173d1b29024731e7db7d5
pei <shhxshh.1010@gmail.com>
2019-01-09 00:06:43 +0900

    ch6 6.2.2まで完了しました。 存在性のvalidatesの設定を追加。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+    # validates :name, :presence => true # これも同じです。
+    # validates(シンボル, ハッシュ)
+    # validates(:name, {:presence => true}) # これもいける
+    validates(:name, {:presence => true}) # これもいける
+    validates :email, presence: true
+
+
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,24 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup 
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name ="   "
+    assert_not @user.valid?
+  end
+
+  test "email shoule be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+
+
+
 end
