e9276835d700eadf3d12385f9ca74046255fb379
pei <shhxshh.1010@gmail.com>
2019-01-13 09:55:17 +0900

    ch6.2.5 emailをuniqueにする。DBも側もindex,uniqueにする。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,19 @@ class User < ApplicationRecord
     # validates :name, :presence => true # これも同じです。
     # validates(シンボル, ハッシュ)
     # validates(:name, {:presence => true}) # これもいける
-    validates(:name, {:presence => true, length: { maximum: 50 } } ) # これもいける
+    
+    validates(:name, {:presence => true ,
+                      length: { maximum: 50 }
+                      } ) # これもいける
     # validates :name, presence: true, length: { maximum: 50}
     
 
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 } ,format: {with: VALID_EMAIL_REGEX}
 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,
+                      length: { maximum: 255 },
+                      format: {with: VALID_EMAIL_REGEX},
+                      uniqueness: {case_sensitive: false}
 
 
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -43,5 +43,12 @@ class UserTest < ActiveSupport::TestCase
       end
   end
 
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = duplicate_user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?  
+  end
+
 
 end
