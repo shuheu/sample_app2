76fb32da74087cdeb8945da811347fb742211cfe
pei <shhxshh.1010@gmail.com>
2019-01-23 18:59:30 +0900

    chp8.2.3まで完了 sessionsコントローラ作成、login実装
    login実装（ログインフォーム、loginメソッド）

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+
+  def new
+  end #new 
+
+  def create 
+    #ここにユーザ認証する
+    # DBから取り出す
+    user = User.find_by(email: params[:session][:email].downcase)
+    #認証
+    if user && user.authenticate(params[:session][:password])
+      # 成功
+      log_in user
+      redirect_to user # user_url(user)
+    else
+      #失敗
+      flash.now[:danger] = "ログイン失敗 Emailとpasswordの組み合わせが正しくありません。"
+      render 'new'
+    end #if
+
+    
+    
+    
+    
+ 
+  end #create
+
+  def destory
+
+  end #destory
+
+end
