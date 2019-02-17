9ce50b732ce72fe47248ef739b91aed2889f8f76
pei <shhxshh.1010@gmail.com>
2019-02-17 22:21:10 +0900

    chp10.2.1 beforeアクションの設定 editとupdate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new 
     # debugger #ここではからのuserをわたしている？
@@ -45,6 +47,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+# beforeアクション
 
-
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
