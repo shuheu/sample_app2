f09688fa90112c0ffa86d88c79b7ee19e4b0572b
pei <shhxshh.1010@gmail.com>
2019-02-18 20:23:07 +0900

    12.1.3 createアクションでパスワード再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
     attr_accessor :activation_token
+    attr_accessor :reset_token
     # before_save {self.email = self.email.downcase }
     # before_save {self.email.downcase! }
     # before_save {email.downcase! }
@@ -81,6 +82,19 @@ class User < ApplicationRecord
     def send_activation_email
       UserMailer.account_activation(self).deliver_now
     end
+
+      # パスワード再設定のメールを送信する
+      def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+      end
+      # パスワード再設定の属性を設定する
+      def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest,  User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+
+        
+      end
 
 
 
