6ff2029cfb89737b065c8eb18932ae029539f5a5
pei <shhxshh.1010@gmail.com>
2019-02-18 17:39:24 +0900

    11.3.2 editアクションで有効化

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,14 +9,18 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     #認証
     if @user && @user.authenticate(params[:session][:password])
-      # 成功
-      log_in @user
-
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user) # 三項演算子    
-      
-      # redirect_to @user # user_url(user)
-      redirect_back_or @user #デフォルトは個人ページへ、事前にリクエストが保存されていればそこへいく
-
+      if user.activated?
+          # 成功
+          log_in @user
+          params[:session][:remember_me] == '1' ? remember(@user) : forget(@user) # 三項演算子    
+          # redirect_to @user # user_url(user)
+          redirect_back_or @user #デフォルトは個人ページへ、事前にリクエストが保存されていればそこへいく
+        else
+          message  = "Account not activated. "
+          message += "Check your email for the activation link."
+          flash[:warning] = message
+          redirect_to root_url
+      end
     else
       #失敗
       flash.now[:danger] = "ログイン失敗 Emailとpasswordの組み合わせが正しくありません。"
